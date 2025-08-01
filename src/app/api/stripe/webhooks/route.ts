2d7095f2cdd72de516d61bb84bdf24fc010d1bc4
KylerZan <kzanuck25@windwardschool.org>
2025-07-31 22:12:24 -0700

    fix(stripe): Downgrade Stripe API version to '2024-04-10' for compatibility with existing features

--- src/app/api/stripe/webhooks/route.ts
+++ src/app/api/stripe/webhooks/route.ts
@@ -12,7 +12,7 @@ const {
 } = getServerEnv();
 
 const supabase = createClient(NEXT_PUBLIC_SUPABASE_URL!, SUPABASE_SERVICE_ROLE_KEY!);
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, { apiVersion: '2024-06-20' });
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, { apiVersion: '2024-04-10' });
 
 export async function POST(req: Request) {
   const body = await req.text();
